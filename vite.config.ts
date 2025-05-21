0f5dc2deab12e4b83e616491ed0dd87c956797ea
Umut Akturk <84997345+UmutAkturk14@users.noreply.github.com>
2025-05-21 22:51:04 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@utils/*": path.resolve(__dirname, "src/utils/*"),
+      "@types/*": path.resolve(__dirname, "src/types/*"),
+      "@interfaces/*": path.resolve(__dirname, "src/interfaces/*"),
+      "@interfaces": path.resolve(__dirname, "src/interfaces/index.ts"),
+      "@api/*": path.resolve(__dirname, "src/api/*"),
+      "@store/*": path.resolve(__dirname, "src/store/*"),
+      "@config/*": path.resolve(__dirname, "src/config/*"),
+      "@helpers/*": path.resolve(__dirname, "src/helpers/*"),
+    },
+  },
+});
